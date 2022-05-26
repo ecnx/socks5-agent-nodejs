01c20b2734850a80c1e6b174450f659112e74df3
ecnx <50424713+ecnx@users.noreply.github.com>
2022-05-26 18:21:36 +0000

    Update socks-proxy.js

--- socks-proxy.js
+++ socks-proxy.js
@@ -1,135 +1,251 @@
-/* Socks-5 Agent Library */
+/* Socks Proxy Connect Library */
 
 /* jshint esversion: 6 */
 /* jshint node: true */
 "use strict";
 
-const net = require('net');
+const net = require('net'),
+    connect_timeout = require('./connect-timeout');
 
 module.exports = {
     connect: connect
 };
 
-/* --- Socks Connect --- */
+/* --- Proxy Socket Functions --- */
 
-function connect_plain(context) {
-    context.socket.connect(context.options.proxy.port, context.options.proxy.host, (err) => {
+function to_timeout_or_null(input) {
+    return Number.isInteger(input) && input > 0 ? input : null;
+}
+
+function connect_socket(context, endpoint, onerror, onsuccess) {
+    const timeout = to_timeout_or_null(context.options.timeout);
+
+    connect_timeout.connect({
+        endpoint: endpoint,
+        timeout: timeout
+    }, (err, socket, timedout) => {
         if (err) {
-            complete(context, err);
+            onerror(err, socket, timedout);
         } else {
-            console.log('socks5 connected.');
-            handshake(context, () => {
-                context.progress = 'connected';
-                complete(context, null);
+            if (!context.done) {
+                context.socket = socket;
+                if (timeout !== null) {
+                    context.socket.setTimeout(timeout);
+                }
+                context.socket.on('error', (err) => {
+                    destroy_socket(context);
+                    onerror(err);
+                }).on('close', () => {
+                    destroy_socket(context);
+                    onerror(new Error('Socket closed'));
+                }).on('timeout', () => {
+                    destroy_socket(context);
+                    onerror(new Error('Proxy connect timed out'), null, true);
+                });
+                onsuccess();
+            }
+        }
+    });
+}
+
+function recv_buffer(context, callback) {
+    context.socket.once('data', (chunk) => {
+        const data = Buffer.from(chunk, 'binary');
+        callback(data);
+    });
+}
+
+function destroy_socket(context) {
+    try {
+        context.socket.destroy();
+    } catch (unused) {}
+}
+
+/* --- Proxy Finish Stage --- */
+
+function proxy_finish(context, callback) {
+    if (!context.done) {
+        context.socket.removeAllListeners('error');
+        context.socket.removeAllListeners('close');
+        context.socket.removeAllListeners('timeout');
+        context.socket.setTimeout(0);
+        callback(null, context.socket);
+    }
+}
+
+/* --- Proxy Authentication Stage --- */
+
+function proxy_authenticate(context, credentials, onerror, onsuccess) {
+    if (!context.done) {
+        console.log('performing proxy auth....');
+        const userbuf = Buffer.from(credentials.username, 'utf8');
+        if (userbuf.length > 255) {
+            onerror(new Error('Proxy username is too long'));
+            return;
+        }
+        const passbuf = Buffer.from(credentials.password, 'utf8');
+        if (passbuf.length > 255) {
+            onerror(new Error('Proxy password is too long'));
+            return;
+        }
+        recv_buffer(context, (data) => {
+            if (data.length == 2 && data[0] == 1 && data[1] == 0) {
+                onsuccess();
+            } else {
+                onerror(new Error('Proxy authentication failed'));
+            }
+        });
+        context.socket.write(Buffer.concat([
+            Buffer.from([1, userbuf.length]),
+            userbuf,
+            Buffer.from([passbuf.length]),
+            passbuf
+        ]));
+    }
+}
+
+/* --- Proxy Request Stage --- */
+
+function proxy_request(context, endpoint, onerror, onsuccess) {
+    if (!context.done) {
+        console.log('performing proxy request...');
+        const hostbuf = Buffer.from(endpoint.host, 'utf8');
+        if (hostbuf.length > 255) {
+            onerror(new Error('Endpoint host is too long'));
+            return;
+        }
+        recv_buffer(context, (data) => {
+            if (data.length >= 2 && data[0] == 5 && data[1] == 0) {
+                onsuccess();
+            } else {
+                onerror(new Error('Proxy request failed'));
+            }
+        });
+        context.socket.write(Buffer.concat([
+            Buffer.from([5, 1, 0, 3, hostbuf.length]),
+            hostbuf,
+            Buffer.from([endpoint.port >> 8, endpoint.port & 0xff])
+        ]));
+    }
+}
+
+/* --- Proxy Handshake Stage --- */
+
+function proxy_handshake(context, credentials, onerror, onsuccess) {
+    if (!context.done) {
+        console.log('performing proxy handshake...');
+        const auth_method = credentials ? 2 : 0;
+        recv_buffer(context, (data) => {
+            if (data.length == 2 && data[0] == 5 && data[1] == auth_method) {
+                onsuccess();
+            } else {
+                onerror(new Error('Proxy handshake failed'));
+            }
+        });
+        context.socket.write(Buffer.from([5, 1, auth_method]));
+    }
+}
+
+/* --- Connect via Proxy Internal --- */
+
+function connect_next(context, callback) {
+    proxy_handshake(context, context.options.credentials, callback, () => {
+        if (context.options.credentials) {
+            proxy_authenticate(context, context.options.credentials, callback, () => {
+                proxy_request(context, context.options.endpoint, callback, () => {
+                    proxy_finish(context, callback);
+                });
+            });
+        } else {
+            proxy_request(context, context.options.endpoint, callback, () => {
+                proxy_finish(context, callback);
             });
         }
     });
 }
 
-function connect(options, callback) {
-    if (!options.hasOwnProperty('endpoint')) {
-        throw new Error('Endpoint not found in options');
-    }
-    if (!options.endpoint.hasOwnProperty('host') ||
-        !options.endpoint.hasOwnProperty('port')) {
-        throw new Error('Endpoint is invalid');        
-    }
-    const context = {
-        options: options,
-        socket: new net.Socket(),
-        progress: 'connect',
-        callback: callback
-    };
-    if (options.ontimeout) {
-        context.ontimeout = options.ontimeout;
-    }
-    setup_timeout(context);
-    context.socket.on('error', (err) => {
-        complete(context, err);
-    });
-    context.socket.on('close', () => {
-        complete(context, new Error('Socks5 socket closed'));
-    });
-    connect_plain(context);
-}
-
-/* --- Socks Session Processing --- */
-
-function request(context, callback) {
-    context.progress = 'request';
-    const host = context.options.endpoint.host;
-    const port = context.options.endpoint.port;
-    context.socket.once('data', function(chunk) {
-        const data = Buffer.from(chunk, 'binary');
-        if (data.length >= 2 && data[0] == 5 && data[1] == 0) {
-            console.log('socks5 request success.');
-            callback(null);
-        } else {
-            context.socket.destroy();
-            callback(new Error('Socks5 request failed'));
-        }
-    });
-    const array = [5, 1, 0, 3, host.length];
-    for (let i = 0; i < host.length; i++) {
-        array.push(host.charCodeAt(i));
-    }
-    array.push((port & 0xff00) >> 8);
-    array.push(port & 0xff);
-    context.socket.write(Buffer.from(array));
-}
-
-function handshake(context, callback) {
-    context.progress = 'handshake';
-    context.socket.once('data', function(chunk) {
-        const data = Buffer.from(chunk, 'binary');
-        if (data.length == 2 && data[0] == 5 && data[1] == 0) {
-            console.log('socks5 handshake success.');
-            request(context, callback);
-        } else {
-            callback(new Error('Socks5 handshake failed'));
-        }
-    });
-    context.socket.write(Buffer.from([5, 1, 0]));
-}
-
-/* --- Socks Finalizing --- */
-
-function complete(context, err) {
-    const callback = context.callback;
-    context.callback = null;
-    if (context.socket !== null) {
-        const socket = context.socket;
-        context.socket = null;
-        socket.removeAllListeners('error');
-        socket.removeAllListeners('close');
-        if (err) {
-            socket.destroy();
-            callback(err);
-        } else {
-            callback(null, socket);
-        }
-    } else if (err) {
-        callback(err);
+function connect_internal(context, callback) {
+    if (context.options.bridge) {
+        console.log('connecting proxy via bridge...');
+        connect_socket(context, context.options.bridge, callback, () => {
+            proxy_handshake(context, null, callback, () => {
+                proxy_request(context, context.options.proxy, callback, () => {
+                    connect_next(context, callback);
+                });
+            });
+        });
     } else {
-        callback(new Error('Socks5 socket lost'));
-    }
-    if (context.watchdog) {
-        clearTimeout(context.watchdog);
-        context.watchdog = null;
+        console.log('connecting proxy direct...');
+        connect_socket(context, context.options.proxy, callback, () => {
+            connect_next(context, callback);
+        });
     }
 }
 
-/* --- Timeout Managment --- */
+/* --- Options Validation --- */
 
-function setup_timeout(context) {
-    if (context.options.hasOwnProperty('timeout')) {
-        context.watchdog = setTimeout(() => {
-            if (context.progress !== 'connected') {
-                if (context.ontimeout) {
-                    context.ontimeout();
-                }
-                complete(context, new Error('Socks5 connect timeout'));
-            }
-        }, context.options.timeout);
+function isport(value) {
+    return Number.isInteger(value) && value >= 0 && value < 65536;
+}
+
+function options_validate(options) {
+    if (!options.endpoint) {
+        return new Error('Endpoint option required');
     }
+
+    if (typeof options.endpoint.host !== 'string' ||
+        !isport(options.endpoint.port)) {
+        return new Error('Endpoint option is invalid');
+    }
+
+    if (!options.proxy) {
+        return new Error('Proxy option required');
+    }
+
+    if (typeof options.proxy.host !== 'string' ||
+        !isport(options.proxy.port)) {
+        return new Error('Proxy option is invalid');
+    }
+
+    if (options.bridge) {
+        if (typeof options.proxy.host !== 'string' ||
+            !isport(options.proxy.port)) {
+            return new Error('Proxy option is invalid');
+        }
+    }
+
+    if (options.credentials) {
+        if (typeof options.credentials.username !== 'string' ||
+            typeof options.credentials.password !== 'string') {
+            return new Error('Proxy option is invalid');
+        }
+    }
+
+    return null;
+}
+
+/* --- Connect via Proxy Task --- */
+
+function connect(options, callback) {
+    const verr = options_validate(options);
+    if (verr) {
+        callback(verr);
+        return;
+    }
+
+    const context = {
+        done: false,
+        options: options
+    };
+
+    connect_internal(context, (err, socket, timedout) => {
+        const endflag = !context.done;
+        context.done = true;
+        if (err) {
+            destroy_socket(context);
+        }
+        if (endflag) {
+            callback(err, socket, timedout);
+        }
+    });
 }
