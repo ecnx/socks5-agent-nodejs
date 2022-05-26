1c9b1f3f62f2a1c4b50ba06ebe9b3159ead5cdc9
ecnx <50424713+ecnx@users.noreply.github.com>
2022-05-26 18:21:59 +0000

    Update socks-http2-agent.js

--- socks-http2-agent.js
+++ socks-http2-agent.js
@@ -1,37 +1,39 @@
-/* Socks Proxy H2 Agent */
+/* Socks Proxy Http2 Agent */
 
 /* jshint esversion: 6 */
 /* jshint node: true */
 "use strict";
 
-const http = require('http'),
-    tls = require('tls'),
+const tls = require('tls'),
     http2 = require('http2'),
-    SocksProxy = require('./socks-proxy');
+    socks_proxy = require('./socks-proxy');
 
-function SocksProxyH2(options, selectProxyEndpoint) {
+module.exports = SocksHttp2Agent;
+
+/* --- Http2 Agent Creation --- */
+
+function SocksHttp2Agent(options, selectProxyEndpoint) {
     this.pool = [];
 
-    if (options.hasOwnProperty('proxy')) {
-        if (!options.proxy.hasOwnProperty('host') ||
-            options.proxy.hasOwnProperty('port')) {
-            throw new Error('Proxy endpoint is invalid');
-        }
+    if (options.proxy) {
         this.proxy = options.proxy;
     } else if (selectProxyEndpoint) {
         this.selectProxyEndpoint = selectProxyEndpoint;
         this.proxy = null;
     } else {
-        throw new Error('Proxy config is invalid');
+        throw new Error('Proxy option is required');
     }
 
-    if (options.hasOwnProperty('connectTimeout')) {
-        this.connectTimeout = options.connectTimeout;
+    this.bridge = options.bridge ? options.bridge : null;
+    this.credentials = options.credentials ? options.credentials : null;
+
+    if (isNaN(options.connectTimeout)) {
+        this.connectTimeout = 10000;
     } else {
-        this.connectTimeout = 5000;
+        this.connectTimeout = options.connectTimeout;
     }
 
-    if (options.hasOwnProperty('socketTimeout')) {
+    if (Number.isInteger(options.socketTimeout) && options.socketTimeout >= 0) {
         this.socketTimeout = options.socketTimeout;
     } else {
         this.socketTimeout = 60000;
@@ -44,147 +46,174 @@ function SocksProxyH2(options, selectProxyEndpoint) {
     }
 }
 
-SocksProxyH2.prototype.findClient = function(options, callback) {
+/* --- Http2 Agent Clients Managment --- */
+
+SocksHttp2Agent.prototype.findClient = function(options, callback) {
+    if (!options.endpoint) {
+        callback(new Error('Endpoint option is required'));
+        return;
+    }
+    if (typeof options.endpoint.host !== 'string' ||
+        !Number.isInteger(options.endpoint.port)) {
+        callback(new Error('Endpoint option is invalid'));
+        return;
+    }
+    if (typeof options.scheme !== 'string') {
+        callback(new Error('Http2 :scheme must be a string'));
+        return;
+    }
+    if (typeof options.authority !== 'string') {
+        callback(new Error('Http2 :authority must be a string'));
+        return;
+    }
     if (this.proxy === null) {
         this.selectProxyEndpoint((err, proxy) => {
             if (err) {
                 callback(err);
             } else {
                 this.proxy = proxy;
-                this.processClient(options, callback);
+                this.findClientInternal(options, callback);
             }
         });
     } else {
-        this.processClient(options, callback);
+        this.findClientInternal(options, callback);
     }
 };
 
-SocksProxyH2.prototype.recycleClient = function(options) {
+SocksHttp2Agent.prototype.findClientInternal = function(options, callback) {
     for (let i = 0; i < this.pool.length; i++) {
         const client = this.pool[i];
-        if (client.enabled &&
+        if (client.ready &&
             client.scheme === options.scheme &&
             client.authority === options.authority) {
-            return client;
+            callback(null, client.session);
+            return;
         }
     }
-    return null;
+    const client = {
+        done: false,
+        ready: false,
+        scheme: options.scheme,
+        authority: options.authority,
+        session: null,
+        tlssocket: null,
+        netsocket: null
+    };
+    this.createNewClient(client, options, (err, session) => {
+        const endflag = !client.done;
+        client.done = true;
+        if (err) {
+            this.destroyClient(client);
+        }
+        if (endflag) {
+            callback(err, session);
+        }
+    });
 };
 
-function run_locked(lock, callback) {
-    if (!lock.locked) {
-        lock.locked = true;
-        callback();
-    }
-}
+/* --- Http2 Agent Clients Creation --- */
 
-SocksProxyH2.prototype.processClient = function(options, callback) {
-    const lock = {};
-    const recycled = this.recycleClient(options);
-    if (recycled !== null) {
-        run_locked(lock, () => {
-            callback(null, recycled.session);
-        });
-        return;
-    }
-    SocksProxy.connect({
+SocksHttp2Agent.prototype.createNewClient = function(client, options, callback) {
+    socks_proxy.connect({
         proxy: this.proxy,
         endpoint: options.endpoint,
         timeout: this.connectTimeout,
-        ontimeout: () => {
-            this.resetProxy();
-        }
-    }, (err, socket) => {
+        bridge: this.bridge,
+        credentials: this.credentials
+    }, (err, socket, timedout) => {
         if (err) {
-            run_locked(lock, () => {
-                callback(err);
-            });
-            return;
-        }
-        socket.setTimeout(this.socketTimeout);
-        if (options.scheme === 'https') {
-            const tlssocket = tls.connect({
-                host: options.authority,
-                servername: options.authority,
-                socket: socket,
-                rejectUnauthorized: this.rejectUnauthorized,
-                ALPNProtocols: ['h2'],
-            });
-            tlssocket.on('close', () => {
-                run_locked(lock, () => {
-                    callback(new Error('TLS Socket closed'));
-                });
-            });
-            tlssocket.once('secureConnect', () => {
-                if (tlssocket.authorized || this.rejectUnauthorized === false) {
-                    this.setupConnection(options, tlssocket, lock, callback);
-                } else {
-                    callback(new Error('TLS Socket unauthorized'));
-                }
-            });
-            tlssocket.on('error', (err) => {
-                run_locked(lock, () => {
-                    callback(err);
-                });
-            });
-        } else {
-            this.setupConnection(options, socket, lock, callback);
-        }
-    });
-};
-
-SocksProxyH2.prototype.setupConnection = function(options, socket, lock, callback) {
-    const client = {
-        enabled: false,
-        scheme: options.scheme,
-        authority: options.authority,
-        session: http2.connect(options.scheme + '://' + options.authority, {
-            createConnection: () => {
-                return socket;
+            if (timedout && this.selectProxyEndpoint) {
+                this.proxy = null;
             }
-        })
-    };
-    client.session.on('error', (err) => {
-        client.enabled = false;
-        client.session.destroy();
-        run_locked(lock, () => {
             callback(err);
-        });
-    }).on('close', (err) => {
-        client.enabled = false;
-        client.session.destroy();
-        run_locked(lock, () => {
-            callback(new Error('Http2 session closed'));
-        });
-    }).on('timeout', (err) => {
-        client.enabled = false;
-        client.session.destroy();
-        run_locked(lock, () => {
-            callback(new Error('Http2 session timed out'));
-        });
-    }).on('connect', () => {
-        run_locked(lock, () => {
-            client.enabled = true;
-            this.pool.push(client);
-            callback(null, client.session);
-        });
+        } else {
+            client.netsocket = socket;
+            client.netsocket.on('error', (err) => {
+                callback(err);
+            }).on('close', () => {
+                callback(new Error('Socket closed'));
+            }).on('timeout', () => {
+                callback(new Error('Socket timed out'));
+            });
+            client.netsocket.setTimeout(this.socketTimeout);
+            if (options.scheme === 'https') {
+                client.tlssocket = tls.connect({
+                    servername: options.authority,
+                    socket: client.netsocket,
+                    rejectUnauthorized: this.rejectUnauthorized,
+                    ALPNProtocols: ['h2'],
+                });
+                client.tlssocket.on('error', (err) => {
+                    callback(err);
+                }).on('close', () => {
+                    callback(new Error('TLS Socket closed'));
+                }).on('timeout', () => {
+                    callback(new Error('TLS Socket timed out'));
+                }).once('secureConnect', () => {
+                    if (client.tlssocket.authorized || this.rejectUnauthorized === false) {
+                        client.socket = client.tlssocket;
+                        this.setupHttp2Session(client, options, callback);
+                    } else {
+                        callback(new Error('TLS Socket unauthorized'));
+                    }
+                });
+            } else {
+                client.socket = client.netsocket;
+                this.setupHttp2Session(client, options, callback);
+            }
+        }
     });
 };
 
-SocksProxyH2.prototype.resetProxy = function() {
-    if (this.hasOwnProperty('selectProxyEndpoint')) {
-        this.proxy = null;
+SocksHttp2Agent.prototype.setupHttp2Session = function(client, options, callback) {
+    if (!client.done) {
+        client.session = http2.connect(options.scheme + '://' + options.authority, {
+            createConnection: () => {
+                return client.socket;
+            }
+        });
+        client.session.on('error', (err) => {
+            callback(err);
+        }).on('close', (err) => {
+            callback(new Error('Http2 Session closed'));
+        }).on('timeout', (err) => {
+            callback(new Error('Http2 Session timed out'));
+        }).once('connect', () => {
+            if (!client.done) {
+                client.ready = true;
+                this.pool.push(client);
+                callback(null, client.session);
+            }
+        });
     }
 };
 
-SocksProxyH2.prototype.destroy = function() {
+/* --- Http2 Agent Cleanup --- */
+
+SocksHttp2Agent.prototype.destroyClient = function(client) {
+    client.ready = false;
+    if (client.session) {
+        try {
+            client.session.destroy();
+        } catch (unused) {}
+    }
+    if (client.tlssocket) {
+        try {
+            client.tlssocket.destroy();
+        } catch (unused) {}
+    }
+    if (client.netsocket) {
+        try {
+            client.netsocket.destroy();
+        } catch (unused) {}
+    }
+};
+
+SocksHttp2Agent.prototype.destroy = function() {
     for (let i = 0; i < this.pool.length; i++) {
         const client = this.pool[i];
-        if (client.enabled) {
-            client.enabled = false;
-            client.session.destroy();
+        if (client.ready) {
+            this.destroyClient(client);
         }
     }
 };
-
-module.exports = SocksProxyH2;
